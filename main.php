2b82e425110547d94d15e1ed8ca591231e255707
Abdelilah <abdouajjouqa@gmail.com>
2024-12-11 12:59:34 +0100

    complet the home page and add new page for Utilisateur

--- main.php
+++ main.php
@@ -9,16 +9,17 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css">
     <script src="https://cdn.tailwindcss.com"></script>
-    <title>Document</title>
+    <title>Car Rental</title>
 </head>
 <body>
     <main class="flex">
-
-        <nav class="h-[100vh] w-[15%] bg-slate-700">
+        <!-- side-bare -->
+        <nav class="h-[100vh] w-[25%] bg-black bg-opacity-95">
             <ul class="flex flex-col gap-4 py-10 px-5 text-white">
-                <li class="p-2 border-b border-gray-500 hover:bg-slate-400 hover:scale-110 hover:duration-500"><a href="#">Page d'Accueille</a></li>
-                <li class="p-2 border-b border-gray-500 hover:bg-slate-400 hover:scale-110 hover:duration-500"><a href="#">Utilisateurs</a></li>
-                <li class="p-2 border-b border-gray-500 hover:bg-slate-400 hover:scale-110 hover:duration-500"><a href="#">Voitures</a></li>
+                <a href="#"><li class="p-2 border-b border-gray-500 hover:bg-slate-400 hover:scale-110 hover:duration-500">Page d'Accueille</li></a>
+                <a href="./utilisateur.php"><li class="p-2 border-b border-gray-500 hover:bg-slate-400 hover:scale-110 hover:duration-500">Utilisateurs</li></a>
+                <a href="#"><li class="p-2 border-b border-gray-500 hover:bg-slate-400 hover:scale-110 hover:duration-500">Voitures</li></a>
+                <a href="#"><li class="p-2 border-b border-gray-500 hover:bg-slate-400 hover:scale-110 hover:duration-500">Contrat</li></a>
             </ul>
             <div class="absolute bottom-2 w-[15%]">
                 <ul class="flex justify-evenly">
@@ -29,10 +30,18 @@
                 </ul>
             </div>
         </nav>
-
-        <!-- <section class="bg-red-100">
-            <h1>Hello</h1>
-        </section> -->
+        <!-- context -->
+        <section class="bg-gradient-to-r from-indigo-500 p-20">
+            <div>
+                <h1 class="text-8xl font-medium">Welcome to a Car Rental Management</h1><br>
+                <h2 class="text-2xl">Manage your Rentals contracts easely</h2>
+            </div>
+            <div class="p-7">
+                <button class="bg-violet-950 px-6 py-1 text-white text-xl rounded shadow-lg shadow-violet-600 hover:scale-110 hover:duration-500 hover:bg-green-900 hover:bg-opacity-85">
+                    <a href="#">Sign-in</a>
+                </button>
+            </div>
+        </section>
     </main>
 </body>
 </html>
